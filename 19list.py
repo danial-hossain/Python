94a5891839ffd6412d618bfa17edce101e83d41b
Danial <danialhossaindani@gmail.com>
2025-11-18 10:11:35 +0600

    tuple

--- 19list.py
+++ 19list.py
@@ -1,13 +1,15 @@
 l = [11, 45, 1, 2, 4, 6, 1, 1]
 print(l)
 # l.append(7)
+#l.sort()
 # l.sort(reverse=True)
 # l.reverse()
 # print(l.index(1))
 # print(l.count(1))
 # m = l.copy()
 # m[0] = 0
-# l.insert(1, 899)
+l.insert(1, 899)
+print(l)
 m = [900, 1000, 1100]
 k = l + m
 # print(k)
