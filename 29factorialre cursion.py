02f3e03de67b30801a7ab9791e98512d77d40037
Danial <danialhossaindani@gmail.com>
2025-11-18 15:12:46 +0600

    recursion

---
+++ 29factorialre cursion.py
@@ -0,0 +1,10 @@
+def factorial(num): 
+    if (num == 1 or num == 0):
+        return 1
+    else:
+        return (num * factorial(num - 1)) 
+  
+# Driver Code 
+num = 7; 
+print("Number: ",num)
+print("Factorial: ",factorial(num))
